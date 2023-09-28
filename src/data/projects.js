7efafea06684b95812acceee4d4f607c14f9905c
Sanskar Masurkar <sanskarsrs9@gmail.com>
2023-09-28 16:07:49 +0530

    first

---
+++ src/data/projects.js
@@ -0,0 +1,33 @@
+const projects = [
+  {
+    id: 1,
+    title: "Uni-Lab",
+    image: "ml-lab.png",
+
+    category: "Web Development",
+    tags: ["Bootstrap"],
+    description:
+      "Website for research interinstitutional group Machine Learning, Computer Vision, and Dynamic Systems",
+  },
+  {
+    id: 2,
+    title: "SmartChange",
+    image: "smartchange.png",
+    category: "Machine Learning",
+    tags: ["Python", "Vue.js", "Flask", "PyTorch"],
+    description:
+      "AI-powered web application able to track changes in the urban landscape.",
+  },
+  {
+    id: 3,
+    title: "SmartMonitor",
+    image: "smartmonitor.png",
+    url: "https://github.com/xtenzQ/SmartMonitor",
+    category: "Machine Learning",
+    tags: ["C#", "ASP.NET Core", "OpenCV", "Tensorflow"],
+    description:
+      "Detects damaged building by natural disasters and get a brief information about it.",
+  },
+];
+
+export default projects;
---
+++ src/data/skills.js
@@ -0,0 +1,91 @@
+const skills = [
+  {
+    id: 1,
+    title: "Back End",
+    image: "backend_blue.png",
+    languages: [
+      {
+        id: 1,
+        title: "Java",
+        css: "java",
+      },
+      {
+        id: 2,
+        title: "Groovy",
+        css: "groovy",
+      },
+      {
+        id: 3,
+        title: "Apache Solr",
+        css: "solr",
+      },
+      {
+        id: 4,
+        title: "Spring Data",
+        css: "spring",
+      },
+      {
+        id: 5,
+        title: "Docker",
+        css: "docker",
+      },
+    ],
+    description:
+      "We develop web apps using JVM languages and work closely with NoSQL search engines",
+  },
+  {
+    id: 2,
+    title: "Front End",
+    image: "frontend_blue.png",
+    languages: [
+      {
+        id: 1,
+        title: "JavaScript",
+        css: "js",
+      },
+      {
+        id: 2,
+        title: "Vue.js",
+        css: "vuejs",
+      },
+      {
+        id: 3,
+        title: "Bootstrap",
+        css: "bootstrap",
+      },
+      {
+        id: 4,
+        title: "Bulma",
+        css: "bulma",
+      },
+    ],
+    description:
+      "We have passion for designing webapps and deal most of work with Reactjs and other js frameworks",
+  },
+  {
+    id: 3,
+    title: "Machine Learning",
+    image: "ai_blue.png",
+    languages: [
+      {
+        id: 1,
+        title: "Tensorflow",
+        css: "tensorflow",
+      },
+      {
+        id: 2,
+        title: "PyTorch",
+        css: "pytorch",
+      },
+      {
+        id: 3,
+        title: "OpenCV",
+        css: "opencv",
+      },
+    ],
+    description:
+      "We’ve always been huge believers in exploring and experimenting with new technologies, ideas and trends.",
+  },
+];
+
+export default skills;
